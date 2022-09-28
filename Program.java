31dd8b13b4e755fc4e63c1972851885d257b9f51
glULTRA <7amakurd2002@gmail.com>
2022-09-28 18:58:45 +0300

    Stack has been built from scratch, not using any external array type.

--- Program.java
+++ Program.java
@@ -1,8 +1,18 @@
+import java.util.LinkedList;
+
 /**
  * Program
  */
 public class Program {
     public static void main(String[] args) {
+        Stack<String> stack = new Stack<String>(2); // size of the stack is 2
+        stack.push("Text 1");
+        stack.push("Text 2");
+        stack.push("Text 3"); // excpect to give error!
+        stack.pop();
+        stack.push("Newest data replaced to text 2 !");
         
+        // Peek shows the last element (The most recent) stored in the queue.
+        System.out.println(stack.peek()); 
     }   
 }
---
+++ Stack.java
@@ -0,0 +1,54 @@
+import java.lang.reflect.Array;
+import java.util.LinkedList;
+
+public class Stack<T> {
+    private final T[] m_data;
+    private final int m_capacity;
+    private int m_top;
+
+    public Stack(final int size){
+
+        this.m_data = (T[]) new Object[size];
+        this.m_capacity = size;
+        this.m_top = -1;
+    }
+
+    public void push(T data){
+        if(isFull()){
+            System.out.println("Can't add a new item because the maximum size of the stack is " + m_capacity + " ! ");
+            return;
+        }
+        m_data[++m_top] = data;
+    }
+
+    public void pop(){
+        m_data[m_top--] = null;
+    }
+
+    public T get(int index){
+        try {
+            if(m_data[index] == null){
+                System.out.println("Element must be deleted or not added yet!");
+                // return m_data[m_top];
+                return null;
+            }
+        } catch (Exception e) {
+            System.out.println("Please don't get outside of the range!");
+            return null;
+        }
+
+        return m_data[index];
+    }
+
+    public boolean isFull(){
+        return m_top == m_capacity-1; 
+    }
+
+    public boolean isEmpty(){
+        return m_top == 0;
+    }
+
+    public T peek(){
+        return m_data[m_top];
+    }
+}
